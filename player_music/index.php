48ea3badbe5103d41c2f0903eaa33eef93ce9785
Gabriel.Brito <gabrielbritodacruz@gmail.com>
2020-02-16 20:52:26 -0300

    Projeto do Spotify

---
+++ player_music/index.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="_css/reset.css">
+<link rel="stylesheet" href="_css/index.css">
+
+<div class="wrap">
+
+	<div class="container">
+		<div class="container-content">
+			<h1>Música para todos</h1>
+
+			<h4>Milhões de músicas à sua escolha. E nem precisa de cartão de crédito.</h4>
+
+			<a href="">Obtenha o Spotify Free</a>
+		</div>
+	</div>
+
+</div>
